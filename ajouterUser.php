a6d6eed0dc24920e638b12382bd01a4fe5a501ba
akiro9 <super-boy_2005@hotmail.com>
2019-09-07 15:58:02 +0200

    php

--- ajouterUser.php
+++ ajouterUser.php
@@ -4,6 +4,6 @@ include 'User.php';
 
 $add = new users();
 
-$add->ajouterUser($_POST["UN"],$_POST["PW"],$_POST["FN"],$_POST["Dt"],$_POST["Gender"],$_POST["Country"]);
+$add->ajouterUser($_POST["UN"],$_POST["PW"],$_POST["FN"],$_POST["LN"],$_POST["Adr"],$_POST["Dt"]);
 
 ?>
